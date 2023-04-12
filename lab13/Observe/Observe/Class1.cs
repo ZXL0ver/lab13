587a3d7c512b1125dde9c08f24a89ddfdbe9ad37
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:24:25 +0300

    Add files via upload

---
+++ lab13/Observe/Observe/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observe
+{
+    interface IObserver
+    {
+        void Update(List<Item> items);
+    }
+    class Magnet
+    {
+        private List<Item> items;  
+        private List<IObserver> observers;  
+        public int radius;
+        public int x, y;
+        public Magnet(int radius)
+        {
+            this.items = new List<Item>();
+            this.observers = new List<IObserver>();
+            this.radius = radius;
+            x=0; y=0;
+        }
+        public void goX(int q, RichTextBox w)
+        {
+            x += q;
+            w.Text += this.x +" " +this.y + "\n";
+        }
+        public void goY(int q, RichTextBox w)
+        {
+            y += q;
+            w.Text += this.x + " " + this.y + "\n";
+        }
+        public void AddItem(Item item, RichTextBox w)
+        {
+            this.items.Add(item);
+           
+        }
+        public void Attach(IObserver observer)
+        {
+            this.observers.Add(observer);
+        }
+        public void Detach(IObserver observer)
+        {
+            this.observers.Remove(observer);
+        }
+        public void NotifyObservers(RichTextBox w)
+        {
+            List<Item> ironItems = new List<Item>();
+            foreach (Item item in this.items)
+            {
+                if (item.distanceToPlayer(this.x,this.y) <= this.radius)
+                {
+                    ironItems.Add(item);
+                    w.Text += "somethtng get magnit \n";
+                }
+            }
+            foreach (IObserver observer in this.observers)
+            {
+                observer.Update(ironItems);
+            }
+        }
+    }
+    class Item
+    {
+        public string name;  
+        public int x, y;  
+        public int distanceToPlayer(int playerx, int playery)
+        {             
+            int dx = this.x - playerx;
+            int dy = this.y - playery;
+            return (int) Math.Sqrt((dx * dx)+(dy * dy));
+        }
+    }
+    class IronItemsObserver : IObserver
+    {
+        public void Update(List<Item> items)
+        {
+            Console.WriteLine("Iron items in range:");
+            foreach (Item item in items)
+            {
+                Console.WriteLine("- " + item.name);
+            }
+        }
+    }
+}
---
+++ lab13/Observe/Observe/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IronItemsObserver observer = new IronItemsObserver();
+            player.Attach(observer);
+            Item item1 = new Item { name = "Iron scrap", x = 5, y = 5 };
+            player.AddItem(item1,richTextBox2);
+            Item item2 = new Item { name = "Gold egg", x = 10, y = 10 };
+            player.AddItem(item2, richTextBox2);
+            Item item3 = new Item { name = "Iron bar", x = 50, y = 40 };
+            player.AddItem(item3, richTextBox2);
+        }
+        Magnet player = new Magnet(10);
+        private void button1_Click(object sender, EventArgs e)
+        {           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.goX(5,richTextBox1);
+            player.NotifyObservers(richTextBox2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player.goX(-5, richTextBox1);
+            player.NotifyObservers(richTextBox2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            player.goY(5, richTextBox1);
+            player.NotifyObservers(richTextBox2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.goY(-5, richTextBox1);
+            player.NotifyObservers(richTextBox2);
+        }
+    }
+}
